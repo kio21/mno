fe82dfa9bf6f60e5b3f03ae4441c6be7f3d73be1
Igor Kravchenko <kio21@bk.ru>
2017-03-18 19:41:43 +0300

    added appcompat theme, used appcompatactivity

--- MNO/MainActivity.cs
+++ MNO/MainActivity.cs
@@ -1,18 +1,21 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android;
+using Android.Support.V7.App;
+using Android.Support.V4.Widget;
 
 namespace mno
 {
     [Activity(MainLauncher = true)]
-    public class MainActivity : Activity
+    public class MainActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView (Resource.Layout.activity_main);
         }
     }
 }
