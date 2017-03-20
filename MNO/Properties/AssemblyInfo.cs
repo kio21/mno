cdd11017eb8b89d0478a85d36b608263ac5654eb
Igor Kravchenko <kio21@bk.ru>
2017-03-20 15:38:40 +0300

    added main and base activities; added toolbar; added menu icon

--- MNO/Properties/AssemblyInfo.cs
+++ MNO/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MNO")]
+[assembly: AssemblyTitle("mno")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MNO")]
+[assembly: AssemblyProduct("mno")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
---
+++ mno/Activity/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace mno
+{
+    [Activity(MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ScreenOrientation = ScreenOrientation.Portrait)]
+    public class MainActivity : BaseActivity
+    {
+        protected override int LayoutResource
+        {
+            get
+            {
+                return Resource.Layout.activity_main;
+            }
+        }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SupportActionBar.SetHomeAsUpIndicator(Resource.Drawable.ic_menu);
+        }
+    }
+}
+
